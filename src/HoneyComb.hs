b387d0c680bf768054e1bd28696f7bed32d2544d
Grzegorz Majchrzak <gregiem94@gmail.com>
2017-05-31 05:25:34 +0200

    Added functions to check if puzzle are valid or already solved

--- src/HoneyComb.hs
+++ src/HoneyComb.hs
@@ -15,14 +15,14 @@ import System.IO
 -- Data types
 
 data Field = Empty | A | B | C | D | E | F | G deriving (Show, Read, Eq) 
-data Comb a = Comb [[Field]] deriving (Show, Read)
-data Plaster a = Plaster [String] deriving (Show, Read)
+data Comb = Comb { rows :: [[Field]] } deriving (Show, Read)
+data Plaster = Plaster [String] deriving (Show, Read)
+
 avaliableFields = [A,B,C,D,E,F,G]
 
+-- Data converters and accesors
 
--- Data converters
-
-strToPlaster :: String -> Plaster a
+strToPlaster :: String -> Plaster
 strToPlaster combStr = read combStr
 
 charToField :: String -> Field
@@ -37,14 +37,22 @@ combStrToTables :: [String] -> [[Field]]
 combStrToTables [] = []
 combStrToTables (x:xs) = strToFieldTable(x) : combStrToTables(xs)
 
-plasterToComb :: Plaster a -> Comb a
+plasterToComb :: Plaster -> Comb
 plasterToComb (Plaster a) = Comb (combStrToTables a)
+
+-- row - get row with x index
+row :: Comb -> Int -> [Field]
+row c x = (rows c) !! x
+
+-- field - get field from x row and y column
+field :: Comb -> Int -> Int -> Field
+field c i j = ((rows c) !! i) !! j
 
 -- Main functions
 
 removeItem _ [] = []
 removeItem x (y:ys) | x == y    = removeItem x ys
-                    | otherwise = y : removeItem x ys
+ | otherwise = y : removeItem x ys
 
 next :: Field -> Field
 next Empty = Empty
@@ -56,21 +64,62 @@ next E = F
 next F = G
 next G = Empty
 
-isCombOk :: Comb a -> Bool
-isCombOk x = True
+-- Checks if puzzle has every field different than Empty
+isSolved :: Comb -> Bool
+isSolved c = and [Empty `notElem` row | row <- (rows c)]
 
---test = do
+-- Checks if all connected fields have different values in whole Comb
+isCombOk :: Comb -> Bool
+isCombOk x = and [isRowOk x i | i <- [0..(length(rows x)-1)]]
+
+-- Check if all connected fields have different values in row with i index
+isRowOk :: Comb -> Int -> Bool
+isRowOk x i= and [isFieldOk x i j | j <- [0..(length(row x i)-1)]]
+
+-- Check if all connected fields have different values in field (i, j)
+isFieldOk :: Comb -> Int-> Int -> Bool
+isFieldOk x i j | (i == 0 && j == 0) 
+  = isOkBlock [field x i j, field x i (j+1), field x (i+1) j, field x (i+1) (j+1)]
+ | (i == 0 && j == ((length (row x i))-1)) 
+  = isOkBlock [field x i j, field x i (j-1), field x (i+1) j, field x (i+1) (j+1)]
+ | (i == ((length (rows x))-1) && j == 0) 
+  = isOkBlock [field x i j, field x i (j+1), field x (i-1) j, field x (i-1) (j+1)]
+ | (i == ((length (rows x))-1) && j == ((length (row x i))-1)) 
+  = isOkBlock [field x i j, field x i (j-1), field x (i-1) j, field x (i-1) (j+1)]
+ | (i == 0)
+  = isOkBlock [field x i j, field x i (j-1), field x i (j+1), field x (i+1) j, field x (i+1) (j+1)]
+ | (i == ((length (rows x))-1)) 
+  = isOkBlock [field x i j, field x i (j-1), field x i (j+1), field x (i-1) j, field x (i-1) (j-1)]
+ | (i `mod` 2 == 1 && j == 0) 
+  = isOkBlock [field x i j, field x i (j+1), field x (i-1) j, field x (i+1) j]
+ | (i `mod` 2 == 1 && j == (length (row x i)-1)) 
+  = isOkBlock [field x i j, field x i (j-1), field x (i-1) (j-1), field x (i+1) (j-1)]
+ | (i `mod` 2 == 0 && j == 0)
+  = isOkBlock [field x i j, field x i (j+1), field x (i-1) j, field x (i-1) (j+1), field x (i+1) j, field x (i+1) (j+1)]
+ | (i `mod` 2 == 0 && j == (length (row x i)-1)) 
+  = isOkBlock [field x i j, field x i (j-1), field x (i-1) j, field x (i-1) (j+1), field x (i+1) j, field x (i+1) (j+1)]
+ | (i `mod` 2 == 1)
+  = isOkBlock [field x i j, field x i (j-1), field x i (j+1), field x (i-1) (j-1), field x (i-1) j, field x (i+1) (j-1), field x (i+1) j]
+ | otherwise
+  = isOkBlock [field x i j, field x i (j-1), field x i (j+1), field x (i-1) j, field x (i-1) (j+1), field x (i+1) j, field x (i+1) (j+1)]
+ 
+-- Check if all Fields in list have different values or Empty
+isOkBlock :: [Field] -> Bool
+isOkBlock [] = True
+isOkBlock (Empty:xs) = isOkBlock xs
+isOkBlock (x:xs) = (notElem x xs) && (isOkBlock xs)
+  
+-- test = do
 -- list <- [removeItem C avaliableFields]
 -- list <- [removeItem B list]
 -- show list
 
-
 --main - reads file and starts main algorithm
-main = do
-	putStrLn "File name:"
-	name <- getLine
-	handle <- openFile ("../files/" ++ name) ReadMode
-	combStr <- hGetContents handle
-	if (isCombOk (plasterToComb (strToPlaster combStr)))
-		then putStrLn "TAK"
-		else putStrLn "NIE"
+main = do 
+		putStrLn "File name:"
+		name <- getLine
+		handle <- openFile ("../files/" ++ name) ReadMode
+		combStr <- hGetContents handle
+		if (isCombOk (plasterToComb (strToPlaster combStr)))
+			then putStrLn "TAK"
+			else putStrLn "NIE"
